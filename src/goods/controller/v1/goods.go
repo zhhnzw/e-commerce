ff44b7579ad3a1844c122f0ec4d4e42003d38c97
zhhnzw <t>
2020-06-13 18:04:52 +0800

    控制器取值优先取缓存的处理

--- src/goods/controller/v1/goods.go
+++ src/goods/controller/v1/goods.go
@@ -2,51 +2,65 @@ package v1
 
 import (
 	"context"
-	"github.com/grpc-ecosystem/go-grpc-middleware"
-	"goods/conf"
+	"errors"
+	"fmt"
 	"goods/models"
 	"goods/pb"
 	"goods/utils"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"log"
-	"net"
 )
 
-type goodsServer struct{}
+type GoodsServer struct{}
 
-func (s *goodsServer) GetGoodsList(ctx context.Context, request *pb.GoodsRequest) (*pb.GoodsReply, error) {
-	reply, err := models.GetGoodsByType(request)
-	if err != nil {
-		utils.Logf(err, "")
-		return nil, status.Errorf(codes.InvalidArgument, "检查你的参数:%+v", request)
+func (s *GoodsServer) GetGoodsList(ctx context.Context, request *pb.GoodsRequest) (*pb.GoodsReply, error) {
+	// 如果缓存存在，就取出来返回结果
+	// 如果没有缓存再需要后续操作，取得service层的结果，返回响应并set到context，会在缓存中间件把响应缓存起来
+	if value, ok := utils.InterceptorKeyGoodsList[utils.CacheKeyPrefix+"/pb.Goods/GetGoodsList"]; ok {
+		// 命中缓存时,直接return
+		return value, nil
+	} else {
+		reply, err := models.GetGoodsByType(request)
+		if err != nil {
+			utils.Logf(err, "")
+			return nil, status.Errorf(codes.InvalidArgument, "检查你的参数:%+v", request)
+		}
+		return reply, nil
 	}
-	return reply, nil
 }
 
-func (s *goodsServer) GetGoodsDetail(ctx context.Context, request *pb.GoodsRequest) (*pb.GoodsReplyItem, error) {
-	reply, err := models.GetGoods(request)
-	if err != nil {
-		utils.Logf(err, "")
-		return nil, status.Errorf(codes.InvalidArgument, "检查你的参数:%+v", request)
+// 更新热门商品的热度: 修改商品类别下的访问访问量
+func updateHotIndex(model *pb.GoodsReplyItem) error {
+	if len(model.PrimaryType) == 0 || len(model.SecondaryType) == 0 {
+		return errors.New(fmt.Sprintf("model:%+v", model))
 	}
-	return reply, nil
+	hotGoodsPrimaryTypeKey := fmt.Sprintf("goods_hot_primary_type_%s_zset", model.PrimaryType)
+	hotGoodsSecondaryTypeKey := fmt.Sprintf("goods_hot_secondary_type_%s_zset", model.SecondaryType)
+	utils.RedisClient.ZIncrBy(hotGoodsPrimaryTypeKey, 1, model.GoodsUuid)
+	utils.RedisClient.ZIncrBy(hotGoodsSecondaryTypeKey, 1, model.GoodsUuid)
+	return nil
 }
 
-func Run() {
-	lis, err := net.Listen("tcp", ":"+conf.Config.AppPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer(
-		grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(
-				utils.RequestParamInterceptor,
-				utils.VisitInterceptor,
-				utils.CacheInterceptor)))
-	pb.RegisterGoodsServer(s, &goodsServer{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+func (s *GoodsServer) GetGoodsDetail(ctx context.Context, request *pb.GoodsRequest) (*pb.GoodsReplyItem, error) {
+	if value, ok := utils.InterceptorKeyGoodsDetail[utils.CacheKeyPrefix+"/pb.Goods/GetGoodsDetail"]; ok {
+		// 命中缓存时, 更新商品热度，再return
+		if err := updateHotIndex(value); err != nil {
+			utils.Logf(err, "")
+			return value, err
+		}
+		return value, nil
+	} else {
+		reply, err := models.GetGoods(request)
+		if err != nil {
+			utils.Logf(err, "")
+			return nil, status.Errorf(codes.InvalidArgument, "检查你的参数:%+v", request)
+		} else {
+			// 更新商品热度
+			if err := updateHotIndex(reply); err != nil {
+				utils.Logf(err, "")
+				return value, err
+			}
+		}
+		return reply, nil
 	}
 }
