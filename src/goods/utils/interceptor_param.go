0537cee1fa4ad735b704c6a287c894aa251199ee
zhhnzw <747624368@qq.com>
2020-06-13 18:56:44 +0800

    pageSize和pageIndex参数校验更正为只为List类型接口校验

--- src/goods/utils/interceptor_param.go
+++ src/goods/utils/interceptor_param.go
@@ -7,13 +7,16 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"log"
+	"strings"
 )
 
 func RequestParamInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	log.Printf("before handling. req: %+v", req)
 	if request, ok := req.(*pb.GoodsRequest); ok {
-		if request.PageIndex < 1 || request.PageSize < 1 || request.PageSize > 50 {
-			return "", status.Errorf(codes.InvalidArgument, "pageIndex或pageSize参数错误,参数:%+v", request)
+		if strings.Contains(info.FullMethod, "List") {
+			if request.PageIndex < 1 || request.PageSize < 1 || request.PageSize > 50 {
+				return "", status.Errorf(codes.InvalidArgument, "pageIndex或pageSize参数错误,参数:%+v", request)
+			}
 		}
 	}
 	resp, err := handler(ctx, req)
