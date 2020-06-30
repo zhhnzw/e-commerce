bd42c333a33f898979002792f5ce8e66879550c3
zhhnzw <t>
2020-06-30 21:10:30 +0800

    添加注销、修改密码

--- src/web-backend/src/components/index.js
+++ src/web-backend/src/components/index.js
@@ -1,9 +1,11 @@
 import Home from './Home'
 import TableForGoods from './tables/TableForGoods'
 import LoginForm from './forms/LoginForm'
+import UserPopoverForm from './forms/UserPopoverForm'
 
 export default {
     Home,
     TableForGoods,
-    LoginForm
+    LoginForm,
+    UserPopoverForm
 };
