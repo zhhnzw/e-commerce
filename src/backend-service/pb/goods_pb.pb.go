32bacf2026665f3fa953d3be4184976488e701a4
zhhnzw <t>
2020-07-02 21:17:38 +0800

    添加statistic接口

--- src/backend-service/pb/goods_pb.pb.go
+++ src/backend-service/pb/goods_pb.pb.go
@@ -23,18 +23,18 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type GoodsRequest struct {
-	GoodsUuid            string   `protobuf:"bytes,1,opt,name=GoodsUuid,proto3" json:"GoodsUuid,omitempty"`
-	GoodsFrom            string   `protobuf:"bytes,2,opt,name=GoodsFrom,proto3" json:"GoodsFrom,omitempty"`
-	GoodsTypeId          int64    `protobuf:"varint,3,opt,name=GoodsTypeId,proto3" json:"GoodsTypeId,omitempty"`
-	PrimaryType          string   `protobuf:"bytes,4,opt,name=PrimaryType,proto3" json:"PrimaryType,omitempty"`
-	SecondaryType        string   `protobuf:"bytes,5,opt,name=SecondaryType,proto3" json:"SecondaryType,omitempty"`
-	Img                  string   `protobuf:"bytes,6,opt,name=Img,proto3" json:"Img,omitempty"`
-	Imgs                 string   `protobuf:"bytes,7,opt,name=Imgs,proto3" json:"Imgs,omitempty"`
-	IsValid              bool     `protobuf:"varint,8,opt,name=IsValid,proto3" json:"IsValid,omitempty"`
-	Title                string   `protobuf:"bytes,9,opt,name=Title,proto3" json:"Title,omitempty"`
-	Subtitle             string   `protobuf:"bytes,10,opt,name=Subtitle,proto3" json:"Subtitle,omitempty"`
-	Price                int64    `protobuf:"varint,11,opt,name=Price,proto3" json:"Price,omitempty"`
-	PublishDate          string   `protobuf:"bytes,12,opt,name=PublishDate,proto3" json:"PublishDate,omitempty"`
+	GoodsUuid            string   `protobuf:"bytes,1,opt,name=goodsUuid,proto3" json:"goodsUuid,omitempty"`
+	GoodsFrom            string   `protobuf:"bytes,2,opt,name=goodsFrom,proto3" json:"goodsFrom,omitempty"`
+	GoodsTypeId          int64    `protobuf:"varint,3,opt,name=goodsTypeId,proto3" json:"goodsTypeId,omitempty"`
+	PrimaryType          string   `protobuf:"bytes,4,opt,name=primaryType,proto3" json:"primaryType,omitempty"`
+	SecondaryType        string   `protobuf:"bytes,5,opt,name=secondaryType,proto3" json:"secondaryType,omitempty"`
+	Img                  string   `protobuf:"bytes,6,opt,name=img,proto3" json:"img,omitempty"`
+	Imgs                 string   `protobuf:"bytes,7,opt,name=imgs,proto3" json:"imgs,omitempty"`
+	IsValid              bool     `protobuf:"varint,8,opt,name=isValid,proto3" json:"isValid,omitempty"`
+	Title                string   `protobuf:"bytes,9,opt,name=title,proto3" json:"title,omitempty"`
+	Subtitle             string   `protobuf:"bytes,10,opt,name=subtitle,proto3" json:"subtitle,omitempty"`
+	Price                int64    `protobuf:"varint,11,opt,name=price,proto3" json:"price,omitempty"`
+	PublishDate          string   `protobuf:"bytes,12,opt,name=publishDate,proto3" json:"publishDate,omitempty"`
 	PageIndex            int64    `protobuf:"varint,13,opt,name=pageIndex,proto3" json:"pageIndex,omitempty"`
 	PageSize             int64    `protobuf:"varint,14,opt,name=pageSize,proto3" json:"pageSize,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -166,18 +166,18 @@ func (m *GoodsRequest) GetPageSize() int64 {
 }
 
 type GoodsReplyItem struct {
-	GoodsUuid            string   `protobuf:"bytes,1,opt,name=GoodsUuid,proto3" json:"GoodsUuid,omitempty"`
-	GoodsFrom            string   `protobuf:"bytes,2,opt,name=GoodsFrom,proto3" json:"GoodsFrom,omitempty"`
-	GoodsTypeId          int64    `protobuf:"varint,3,opt,name=GoodsTypeId,proto3" json:"GoodsTypeId,omitempty"`
-	PrimaryType          string   `protobuf:"bytes,4,opt,name=PrimaryType,proto3" json:"PrimaryType,omitempty"`
-	SecondaryType        string   `protobuf:"bytes,5,opt,name=SecondaryType,proto3" json:"SecondaryType,omitempty"`
-	Img                  string   `protobuf:"bytes,6,opt,name=Img,proto3" json:"Img,omitempty"`
-	Imgs                 string   `protobuf:"bytes,7,opt,name=Imgs,proto3" json:"Imgs,omitempty"`
-	IsValid              bool     `protobuf:"varint,8,opt,name=IsValid,proto3" json:"IsValid,omitempty"`
-	Title                string   `protobuf:"bytes,9,opt,name=Title,proto3" json:"Title,omitempty"`
-	Subtitle             string   `protobuf:"bytes,10,opt,name=Subtitle,proto3" json:"Subtitle,omitempty"`
-	Price                int64    `protobuf:"varint,11,opt,name=Price,proto3" json:"Price,omitempty"`
-	PublishDate          string   `protobuf:"bytes,12,opt,name=PublishDate,proto3" json:"PublishDate,omitempty"`
+	GoodsUuid            string   `protobuf:"bytes,1,opt,name=goodsUuid,proto3" json:"goodsUuid,omitempty"`
+	GoodsFrom            string   `protobuf:"bytes,2,opt,name=goodsFrom,proto3" json:"goodsFrom,omitempty"`
+	GoodsTypeId          int64    `protobuf:"varint,3,opt,name=goodsTypeId,proto3" json:"goodsTypeId,omitempty"`
+	PrimaryType          string   `protobuf:"bytes,4,opt,name=primaryType,proto3" json:"primaryType,omitempty"`
+	SecondaryType        string   `protobuf:"bytes,5,opt,name=secondaryType,proto3" json:"secondaryType,omitempty"`
+	Img                  string   `protobuf:"bytes,6,opt,name=img,proto3" json:"img,omitempty"`
+	Imgs                 string   `protobuf:"bytes,7,opt,name=imgs,proto3" json:"imgs,omitempty"`
+	IsValid              bool     `protobuf:"varint,8,opt,name=isValid,proto3" json:"isValid,omitempty"`
+	Title                string   `protobuf:"bytes,9,opt,name=title,proto3" json:"title,omitempty"`
+	Subtitle             string   `protobuf:"bytes,10,opt,name=subtitle,proto3" json:"subtitle,omitempty"`
+	Price                int64    `protobuf:"varint,11,opt,name=price,proto3" json:"price,omitempty"`
+	PublishDate          string   `protobuf:"bytes,12,opt,name=publishDate,proto3" json:"publishDate,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -331,41 +331,132 @@ func (m *GoodsReply) GetData() []*GoodsReplyItem {
 	return nil
 }
 
+type GoodsStatisticItem struct {
+	GoodsType            string   `protobuf:"bytes,1,opt,name=goodsType,proto3" json:"goodsType,omitempty"`
+	Count                int64    `protobuf:"varint,2,opt,name=count,proto3" json:"count,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GoodsStatisticItem) Reset()         { *m = GoodsStatisticItem{} }
+func (m *GoodsStatisticItem) String() string { return proto.CompactTextString(m) }
+func (*GoodsStatisticItem) ProtoMessage()    {}
+func (*GoodsStatisticItem) Descriptor() ([]byte, []int) {
+	return fileDescriptor_088a779269859b27, []int{3}
+}
+
+func (m *GoodsStatisticItem) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GoodsStatisticItem.Unmarshal(m, b)
+}
+func (m *GoodsStatisticItem) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GoodsStatisticItem.Marshal(b, m, deterministic)
+}
+func (m *GoodsStatisticItem) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GoodsStatisticItem.Merge(m, src)
+}
+func (m *GoodsStatisticItem) XXX_Size() int {
+	return xxx_messageInfo_GoodsStatisticItem.Size(m)
+}
+func (m *GoodsStatisticItem) XXX_DiscardUnknown() {
+	xxx_messageInfo_GoodsStatisticItem.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GoodsStatisticItem proto.InternalMessageInfo
+
+func (m *GoodsStatisticItem) GetGoodsType() string {
+	if m != nil {
+		return m.GoodsType
+	}
+	return ""
+}
+
+func (m *GoodsStatisticItem) GetCount() int64 {
+	if m != nil {
+		return m.Count
+	}
+	return 0
+}
+
+type GoodsStatisticReply struct {
+	Data                 []*GoodsStatisticItem `protobuf:"bytes,1,rep,name=data,proto3" json:"data,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}              `json:"-"`
+	XXX_unrecognized     []byte                `json:"-"`
+	XXX_sizecache        int32                 `json:"-"`
+}
+
+func (m *GoodsStatisticReply) Reset()         { *m = GoodsStatisticReply{} }
+func (m *GoodsStatisticReply) String() string { return proto.CompactTextString(m) }
+func (*GoodsStatisticReply) ProtoMessage()    {}
+func (*GoodsStatisticReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_088a779269859b27, []int{4}
+}
+
+func (m *GoodsStatisticReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GoodsStatisticReply.Unmarshal(m, b)
+}
+func (m *GoodsStatisticReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GoodsStatisticReply.Marshal(b, m, deterministic)
+}
+func (m *GoodsStatisticReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GoodsStatisticReply.Merge(m, src)
+}
+func (m *GoodsStatisticReply) XXX_Size() int {
+	return xxx_messageInfo_GoodsStatisticReply.Size(m)
+}
+func (m *GoodsStatisticReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_GoodsStatisticReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GoodsStatisticReply proto.InternalMessageInfo
+
+func (m *GoodsStatisticReply) GetData() []*GoodsStatisticItem {
+	if m != nil {
+		return m.Data
+	}
+	return nil
+}
+
 func init() {
-	proto.RegisterType((*GoodsRequest)(nil), "pb.GoodsRequest")
-	proto.RegisterType((*GoodsReplyItem)(nil), "pb.GoodsReplyItem")
-	proto.RegisterType((*GoodsReply)(nil), "pb.GoodsReply")
+	proto.RegisterType((*GoodsRequest)(nil), "pb.goodsRequest")
+	proto.RegisterType((*GoodsReplyItem)(nil), "pb.goodsReplyItem")
+	proto.RegisterType((*GoodsReply)(nil), "pb.goodsReply")
+	proto.RegisterType((*GoodsStatisticItem)(nil), "pb.goodsStatisticItem")
+	proto.RegisterType((*GoodsStatisticReply)(nil), "pb.goodsStatisticReply")
 }
 
 func init() { proto.RegisterFile("pb/goods_pb.proto", fileDescriptor_088a779269859b27) }
 
 var fileDescriptor_088a779269859b27 = []byte{
-	// 385 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x93, 0xcf, 0x6a, 0xdb, 0x40,
-	0x10, 0xc6, 0x2d, 0xcb, 0x7f, 0xc7, 0xb6, 0xea, 0x0e, 0x3d, 0x2c, 0xa6, 0x07, 0x21, 0x4a, 0xd1,
-	0xc9, 0x05, 0xb7, 0x85, 0x3e, 0x80, 0xa9, 0x2b, 0xe8, 0xc1, 0xc8, 0x6e, 0xaf, 0x41, 0xb2, 0x16,
-	0x65, 0x41, 0xb2, 0x36, 0xd2, 0x0a, 0xe2, 0x3c, 0x49, 0x1e, 0x22, 0x0f, 0x95, 0x47, 0x09, 0x3b,
-	0xb6, 0x2c, 0x19, 0x7c, 0xc8, 0x21, 0xc7, 0xdc, 0xf6, 0xfb, 0x7d, 0xf3, 0xed, 0x32, 0x33, 0x2c,
-	0x7c, 0x94, 0xe1, 0xb7, 0x38, 0xcb, 0xa2, 0xe2, 0x46, 0x86, 0x73, 0x99, 0x67, 0x2a, 0xc3, 0xb6,
-	0x0c, 0x9d, 0x47, 0x13, 0xc6, 0x2b, 0x8d, 0x7d, 0x7e, 0x57, 0xf2, 0x42, 0xe1, 0x67, 0x18, 0x92,
-	0xfe, 0x57, 0x8a, 0x88, 0x19, 0xb6, 0xe1, 0x0e, 0xfd, 0x1a, 0x9c, 0xdd, 0xdf, 0x79, 0x96, 0xb2,
-	0x76, 0xc3, 0xd5, 0x00, 0x6d, 0x18, 0x91, 0xd8, 0x1e, 0x24, 0xf7, 0x22, 0x66, 0xda, 0x86, 0x6b,
-	0xfa, 0x4d, 0xa4, 0x2b, 0xd6, 0xb9, 0x48, 0x83, 0xfc, 0xa0, 0x01, 0xeb, 0xd0, 0x0d, 0x4d, 0x84,
-	0x5f, 0x60, 0xb2, 0xe1, 0xbb, 0x6c, 0x1f, 0x55, 0x35, 0x5d, 0xaa, 0xb9, 0x84, 0x38, 0x05, 0xd3,
-	0x4b, 0x63, 0xd6, 0x23, 0x4f, 0x1f, 0x11, 0xa1, 0xe3, 0xa5, 0x71, 0xc1, 0xfa, 0x84, 0xe8, 0x8c,
-	0x0c, 0xfa, 0x5e, 0xf1, 0x3f, 0x48, 0x44, 0xc4, 0x06, 0xb6, 0xe1, 0x0e, 0xfc, 0x4a, 0xe2, 0x27,
-	0xe8, 0x6e, 0x85, 0x4a, 0x38, 0x1b, 0x52, 0xf9, 0x51, 0xe0, 0x0c, 0x06, 0x9b, 0x32, 0x54, 0x64,
-	0x00, 0x19, 0x67, 0xad, 0x13, 0xeb, 0x5c, 0xec, 0x38, 0x1b, 0x51, 0x57, 0x47, 0x41, 0xfd, 0x94,
-	0x61, 0x22, 0x8a, 0xdb, 0x65, 0xa0, 0x38, 0x1b, 0x9f, 0xfa, 0xa9, 0x91, 0x9e, 0x98, 0x0c, 0x62,
-	0xee, 0xed, 0x23, 0x7e, 0xcf, 0x26, 0x94, 0xad, 0x81, 0x7e, 0x51, 0x8b, 0x8d, 0x78, 0xe0, 0xcc,
-	0x22, 0xf3, 0xac, 0x9d, 0xe7, 0x36, 0x58, 0xa7, 0xd5, 0xc8, 0xe4, 0xe0, 0x29, 0x9e, 0xbe, 0x2f,
-	0xe7, 0x6d, 0x97, 0xe3, 0xfc, 0x00, 0xa8, 0x27, 0x8c, 0x5f, 0xa1, 0x13, 0x05, 0x2a, 0x60, 0x86,
-	0x6d, 0xba, 0xa3, 0x05, 0xce, 0x65, 0x38, 0xbf, 0x9c, 0xbf, 0x4f, 0xfe, 0xe2, 0xc9, 0x80, 0x2e,
-	0x19, 0xb8, 0x80, 0xf1, 0x8a, 0x2b, 0x3a, 0xff, 0x15, 0x85, 0xc2, 0x69, 0x23, 0x43, 0xdf, 0x69,
-	0x66, 0x5d, 0xde, 0xe2, 0xb4, 0xf0, 0x17, 0x58, 0x55, 0x66, 0xc9, 0x55, 0x20, 0x92, 0x2b, 0xa9,
-	0x2b, 0x6f, 0x3b, 0x2d, 0xfc, 0x09, 0x1f, 0xaa, 0xe4, 0x9f, 0x4c, 0xbd, 0xf6, 0xc1, 0xb0, 0x47,
-	0xbf, 0xfd, 0xfb, 0x4b, 0x00, 0x00, 0x00, 0xff, 0xff, 0x99, 0xdd, 0xd0, 0xcf, 0x02, 0x04, 0x00,
-	0x00,
+	// 447 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x54, 0x4d, 0x6f, 0xd3, 0x40,
+	0x10, 0xad, 0xe3, 0xa4, 0x4d, 0x26, 0x69, 0x68, 0x87, 0x0a, 0x56, 0x15, 0x07, 0xcb, 0x42, 0x28,
+	0xe2, 0x10, 0xa4, 0x00, 0x12, 0x47, 0x90, 0x2a, 0xda, 0x48, 0x9c, 0x1c, 0xe0, 0x8a, 0xd6, 0xf1,
+	0xca, 0xac, 0x64, 0x67, 0x17, 0xef, 0x5a, 0x22, 0xfc, 0x12, 0x7e, 0x1e, 0xbf, 0x80, 0xdf, 0x80,
+	0x76, 0x1c, 0x7f, 0x89, 0x1c, 0x38, 0x70, 0xec, 0x6d, 0xdf, 0x7b, 0xf3, 0x76, 0xe7, 0xcd, 0xc8,
+	0x86, 0x4b, 0x1d, 0xbf, 0x48, 0x95, 0x4a, 0xcc, 0x17, 0x1d, 0x2f, 0x75, 0xa1, 0xac, 0xc2, 0x81,
+	0x8e, 0xc3, 0x9f, 0x3e, 0xcc, 0x88, 0x8e, 0xc4, 0xb7, 0x52, 0x18, 0x8b, 0x4f, 0x60, 0x42, 0xf8,
+	0x53, 0x29, 0x13, 0xe6, 0x05, 0xde, 0x62, 0x12, 0xb5, 0x44, 0xa3, 0xbe, 0x2f, 0x54, 0xce, 0x06,
+	0x1d, 0xd5, 0x11, 0x18, 0xc0, 0x94, 0xc0, 0xc7, 0xbd, 0x16, 0xeb, 0x84, 0xf9, 0x81, 0xb7, 0xf0,
+	0xa3, 0x2e, 0xe5, 0x2a, 0x74, 0x21, 0x73, 0x5e, 0xec, 0x1d, 0xc1, 0x86, 0x74, 0x43, 0x97, 0xc2,
+	0xa7, 0x70, 0x6e, 0xc4, 0x56, 0xed, 0x92, 0xba, 0x66, 0x44, 0x35, 0x7d, 0x12, 0x2f, 0xc0, 0x97,
+	0x79, 0xca, 0x4e, 0x49, 0x73, 0x47, 0x44, 0x18, 0xca, 0x3c, 0x35, 0xec, 0x8c, 0x28, 0x3a, 0x23,
+	0x83, 0x33, 0x69, 0x3e, 0xf3, 0x4c, 0x26, 0x6c, 0x1c, 0x78, 0x8b, 0x71, 0x54, 0x43, 0xbc, 0x82,
+	0x91, 0x95, 0x36, 0x13, 0x6c, 0x42, 0xe5, 0x15, 0xc0, 0x6b, 0x18, 0x9b, 0x32, 0xae, 0x04, 0x20,
+	0xa1, 0xc1, 0xce, 0xa1, 0x0b, 0xb9, 0x15, 0x6c, 0x4a, 0xa9, 0x2a, 0x40, 0x79, 0xca, 0x38, 0x93,
+	0xe6, 0xeb, 0x0d, 0xb7, 0x82, 0xcd, 0x0e, 0x79, 0x5a, 0xca, 0x4d, 0x4c, 0xf3, 0x54, 0xac, 0x77,
+	0x89, 0xf8, 0xce, 0xce, 0xc9, 0xdb, 0x12, 0xee, 0x45, 0x07, 0x36, 0xf2, 0x87, 0x60, 0x73, 0x12,
+	0x1b, 0x1c, 0xfe, 0x1a, 0xc0, 0xfc, 0xb0, 0x1a, 0x9d, 0xed, 0xd7, 0x56, 0xe4, 0xf7, 0xcb, 0xf9,
+	0xbf, 0xcb, 0x09, 0x5f, 0x01, 0xb4, 0x13, 0xc6, 0x67, 0x30, 0x4c, 0xb8, 0xe5, 0xcc, 0x0b, 0xfc,
+	0xc5, 0x74, 0x85, 0x4b, 0x1d, 0x2f, 0xfb, 0xf3, 0x8f, 0x48, 0x0f, 0xef, 0x00, 0x89, 0xdf, 0x58,
+	0x6e, 0xa5, 0xb1, 0x72, 0xdb, 0xdb, 0x0d, 0xcd, 0xa5, 0xbb, 0x1b, 0x9a, 0xc9, 0x15, 0x8c, 0xb6,
+	0xaa, 0xdc, 0x59, 0xda, 0x8b, 0x1f, 0x55, 0x20, 0x7c, 0x07, 0x0f, 0xfb, 0x37, 0x55, 0x8d, 0x3c,
+	0xef, 0x35, 0xf2, 0xa8, 0x69, 0xa4, 0xf7, 0x60, 0xd5, 0xcc, 0xea, 0xb7, 0x07, 0xa3, 0x5b, 0x27,
+	0xe2, 0x0a, 0x66, 0xb7, 0xc2, 0xd2, 0xf9, 0x83, 0x34, 0x16, 0x2f, 0x3a, 0x01, 0xe8, 0xdb, 0xbe,
+	0x9e, 0xf7, 0x23, 0x85, 0x27, 0xf8, 0x06, 0xe6, 0xb5, 0xe7, 0x46, 0x58, 0x2e, 0xb3, 0x23, 0xae,
+	0x23, 0x83, 0x08, 0x4f, 0xf0, 0x35, 0x3c, 0xa8, 0x9d, 0x77, 0xca, 0xfe, 0xf3, 0x83, 0x6f, 0xe1,
+	0xb2, 0xb6, 0x35, 0x69, 0x8e, 0x18, 0x1f, 0xff, 0x9d, 0xf9, 0x70, 0x43, 0x7c, 0x4a, 0x3f, 0xaf,
+	0x97, 0x7f, 0x02, 0x00, 0x00, 0xff, 0xff, 0xfd, 0x6d, 0x4b, 0xe8, 0xd1, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -383,6 +474,7 @@ type GoodsClient interface {
 	GetGoodsList(ctx context.Context, in *GoodsRequest, opts ...grpc.CallOption) (*GoodsReply, error)
 	GetGoodsDetail(ctx context.Context, in *GoodsRequest, opts ...grpc.CallOption) (*GoodsReplyItem, error)
 	GetGoodsHotList(ctx context.Context, in *GoodsRequest, opts ...grpc.CallOption) (*GoodsReply, error)
+	GetGoodsStatistic(ctx context.Context, in *GoodsRequest, opts ...grpc.CallOption) (*GoodsStatisticReply, error)
 }
 
 type goodsClient struct {
@@ -420,11 +512,21 @@ func (c *goodsClient) GetGoodsHotList(ctx context.Context, in *GoodsRequest, opt
 	return out, nil
 }
 
+func (c *goodsClient) GetGoodsStatistic(ctx context.Context, in *GoodsRequest, opts ...grpc.CallOption) (*GoodsStatisticReply, error) {
+	out := new(GoodsStatisticReply)
+	err := c.cc.Invoke(ctx, "/pb.Goods/GetGoodsStatistic", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GoodsServer is the server API for Goods service.
 type GoodsServer interface {
 	GetGoodsList(context.Context, *GoodsRequest) (*GoodsReply, error)
 	GetGoodsDetail(context.Context, *GoodsRequest) (*GoodsReplyItem, error)
 	GetGoodsHotList(context.Context, *GoodsRequest) (*GoodsReply, error)
+	GetGoodsStatistic(context.Context, *GoodsRequest) (*GoodsStatisticReply, error)
 }
 
 func RegisterGoodsServer(s *grpc.Server, srv GoodsServer) {
@@ -485,6 +587,24 @@ func _Goods_GetGoodsHotList_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Goods_GetGoodsStatistic_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GoodsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GoodsServer).GetGoodsStatistic(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/pb.Goods/GetGoodsStatistic",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GoodsServer).GetGoodsStatistic(ctx, req.(*GoodsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Goods_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "pb.Goods",
 	HandlerType: (*GoodsServer)(nil),
@@ -500,6 +620,10 @@ var _Goods_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetGoodsHotList",
 			Handler:    _Goods_GetGoodsHotList_Handler,
+		},
+		{
+			MethodName: "GetGoodsStatistic",
+			Handler:    _Goods_GetGoodsStatistic_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- src/backend-service/routers/goods.go
+++ src/backend-service/routers/goods.go
@@ -6,7 +6,8 @@ import (
 )
 
 func SetGoodsRouter(router *gin.Engine) {
-	router.GET("/v1/hot", v1.GetHotGoodsList)
+	router.GET("/v1/hot/goods", v1.GetHotGoodsList)
+	router.GET("/v1/statistic/goods", v1.GetGoodsStatistic)
 	goodsRouter := router.Group("/v1/goods")
 	goodsRouter.GET("", v1.GetGoodsList)
 	goodsRouter.GET("/:uuid", v1.GetGoods)
