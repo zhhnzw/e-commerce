4b192f59ee55f48352b4f4215721c9fec3e27ac2
zhhnzw <747624368@qq.com>
2020-07-04 20:08:46 +0800

    添加库存

--- src/goods/controller/v1/goods_test.go
+++ src/goods/controller/v1/goods_test.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	. "github.com/smartystreets/goconvey/convey"
 	"goods/pb"
+	"goods/utils"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"log"
-	"reflect"
 	"testing"
 	"time"
 )
@@ -16,7 +14,7 @@ import (
 const address = "localhost:50051"
 
 func Test(t *testing.T) {
-	Convey("TestGetCommodities", t, func() {
+	Convey("Test", t, func() {
 		// Set up a connection to the server.
 		conn, err := grpc.Dial(address, grpc.WithInsecure())
 		if err != nil {
@@ -26,19 +24,19 @@ func Test(t *testing.T) {
 		goodsClient := pb.NewGoodsClient(conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
-		request := &pb.GoodsRequest{}
-		reply, err := goodsClient.GetGoodsStatistic(ctx, request)
-		So(err, ShouldBeNil)
-		if err != nil {
-			s := status.Convert(err)
-			switch s.Code() {
-			case codes.InvalidArgument:
-				log.Printf("InvalidArgument: %v", s)
-			default:
-				log.Printf("Unexpected type: %v", s)
-			}
-			log.Fatalf("could not greet: %v %s", err, reflect.TypeOf(err))
-		}
-		log.Printf("%+v\n", reply)
+		Convey("Test GetGoodsStatistic", func() {
+			request := &pb.GoodsRequest{}
+			reply, err := goodsClient.GetGoodsStatistic(ctx, request)
+			So(err, ShouldBeNil)
+			utils.CheckRPCError(err)
+			log.Printf("%+v\n", reply)
+		})
+		Convey("Test MakeStockUp", func() {
+			request := &pb.GoodsRequest{GoodsUuid: "b7b10c01-62b8-42c7-a8c4-8efe119cd326"}
+			reply, err := goodsClient.MakeStockUp(ctx, request)
+			So(err, ShouldBeNil)
+			utils.CheckRPCError(err)
+			log.Printf("%+v\n", reply)
+		})
 	})
 }
