721a575aa652f9fbee919d6f3201fd487866c78e
zhhnzw <747624368@qq.com>
2020-07-04 20:26:45 +0800

    移除goods_from字段

--- src/goods/schema.sql
+++ src/goods/schema.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `tb_goods` (
   `id` int NOT NULL AUTO_INCREMENT,
   `goods_uuid` char(36) NOT NULL COMMENT '商品id',
-  `goods_from` enum('unknown','taobao', 'jd') NOT NULL DEFAULT 'unknown' COMMENT '商品来源,如: taobao(淘宝)、jd(京东)',
   `goods_type_id` int NOT NULL DEFAULT 0 COMMENT '商品类型id',
   `primary_type` enum('unknown','clothes', 'pants', 'shoes') DEFAULT 'unknown' COMMENT '一级商品类型,如: clothes(衣服)、pants(裤子)、shoes(鞋子)',
   `secondary_type` enum('unknown','shirt','jacket','casual_pants', 'sports_pants', 'basketball_shoes','casual_shoes') DEFAULT 'unknown' COMMENT '二级商品类型,如: shirt(衬衫)、jacket(夹克)、casual_pants(休闲裤)、sports_pants(运动裤)、basketball_shoes(篮球鞋)、casual_shoes(休闲鞋)',
