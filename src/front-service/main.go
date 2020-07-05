a7b22fafec66c857f6d770678130e93d84c4b3c3
zhhnzw <747624368@qq.com>
2020-07-05 18:51:56 +0800

    update

--- src/front-service/main.go
+++ src/front-service/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"front-service/conf"
-	"front-service/controller/v1"
+	"front-service/models"
 	"front-service/routers"
+	"front-service/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func runServer() {
 	conf.InitConfig()
+	models.InitGorm()
+	utils.InitRedis()
 	gin.SetMode(gin.DebugMode)
 	router := routers.InitRouter()
-	v1.InitGoodsRPCClient()
 	port := conf.Config.AppPort
 	router.Run(":" + port)
 }
