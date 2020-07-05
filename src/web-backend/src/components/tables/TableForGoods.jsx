def90e5c84c7acd91c731f696f24a4cd5d1ac9c6
zhhnzw <t>
2020-07-06 00:06:18 +0800

    修改为按商品类型id号查询

--- src/web-backend/src/components/tables/TableForGoods.jsx
+++ src/web-backend/src/components/tables/TableForGoods.jsx
@@ -38,14 +38,13 @@ class TableForGoods extends React.Component{
     };
 
     updateData(filter) {
-        let primaryType = '';
-        let secondaryType = '';
+        let goodsTypeId = 0;
         if (filter!==undefined && filter.goodsType!==undefined && filter.goodsType.length>0){
-            primaryType = filter.goodsType[0][0];
-            secondaryType = filter.goodsType[0][1];
+            goodsTypeId = filter.goodsType[0];
             this.count = this.countMap[filter.goodsType[0][1]];  // 如果筛选了商品类型，则商品页码也要跟随变动
         }
-        let url = serviceDomain + '/v1/goods?primaryType=' + primaryType + '&secondaryType=' + secondaryType + '&pageSize=' + this.pageSize + '&pageIndex=' + this.pageIndex
+        let url = serviceDomain + '/v1/goods?goodsTypeId=' + goodsTypeId + '&pageSize=' + this.pageSize + '&pageIndex=' + this.pageIndex;
+        this.setState({loading:true});
         get({
             url: url,
             callback: (d) => {
@@ -54,22 +53,24 @@ class TableForGoods extends React.Component{
                     let goodsUuid = d.data.data.data[i].goodsUuid;
                     let secondaryType = d.data.data.data[i].secondaryType;
                     let title = d.data.data.data[i].title;
-                    let price = d.data.data.data[i].price+"元";
+                    let price = d.data.data.data[i].price/100+"元";
+                    let stock = d.data.data.data[i].stock;
                     let publishDate = d.data.data.data[i].publishDate;
                     publishDate = publishDate.replace('T', " ").replace('+08:00', "");
                     let item = {
-                        'key': i, 'title': title, 'goodsType': secondaryType, 'goodsUuid': goodsUuid, 'price':price, 'publishDate': publishDate
+                        'key': i, 'title': title, 'goodsType': secondaryType, 'goodsUuid': goodsUuid, 'price':price, 'publishDate': publishDate, 'stock':stock
                     };
                     data.push(item)
                 }
                 const pagination = {...this.state.pagination};
                 pagination.total = this.count;
-                this.setState({data:data, pagination:pagination});
+                this.setState({data:data, pagination:pagination, loading:false});
             }
         })
     }
 
     getCount() {
+        this.setState({loading:true});
         get({
             url: serviceDomain+'/v1/statistic/goods',
             callback: (d) => {
@@ -83,13 +84,13 @@ class TableForGoods extends React.Component{
                 this.count = count;
                 const pagination = {...this.state.pagination};
                 pagination.total = count;
-                this.setState({pagination:pagination});
+                this.setState({pagination:pagination, loading:false});
             }
         })
     }
 
     handleSearch(value) {
-        console.log(value);
+        this.setState({loading:true});
         get({
             url: serviceDomain+'/v1/goods?pageSize=10&pageIndex=1&goodsUuid='+value,
             callback: (d) => {
@@ -98,18 +99,19 @@ class TableForGoods extends React.Component{
                     let goodsUuid = d.data.data.data[i].goodsUuid;
                     let secondaryType = d.data.data.data[i].secondaryType;
                     let title = d.data.data.data[i].title;
-                    let price = d.data.data.data[i].price+'元';
+                    let price = d.data.data.data[i].price/100+'元';
+                    let stock = d.data.data.data[i].stock;
                     let publishDate = d.data.data.data[i].publishDate;
                     publishDate = publishDate.replace('T', " ").replace('+08:00', "");
                     let item = {
-                        'key': i, 'title': title, 'goodsType': secondaryType, 'goodsUuid': goodsUuid, 'price':price, 'publishDate': publishDate
+                        'key': i, 'title': title, 'goodsType': secondaryType, 'goodsUuid': goodsUuid, 'price':price, 'publishDate': publishDate, 'stock':stock
                     };
                     data.push(item)
                 }
                 this.count = 1;
                 const pagination = {...this.state.pagination};
                 pagination.total = this.count;
-                this.setState({data:data, pagination:pagination});
+                this.setState({data:data, pagination:pagination, loading:false});
             }
         })
     }
@@ -132,11 +134,12 @@ class TableForGoods extends React.Component{
         const hasSelected = selectedRowKeys.length > 0;
         const columns = [{
             title: '商品ID',
+            dataIndex: 'goodsUuid',
             key: 'goodsUuid',
-            sorter: (a, b) => a.goodsUuid - b.goodsUuid,
-            render: (text, record) => (
-                <div>{record.goodsUuid}</div>
-            ),
+            // sorter: (a, b) => a.goodsUuid - b.goodsUuid,
+            // render: (text, record) => (
+            //     <div>{record.goodsUuid}</div>
+            // ),
         }, {
             title: '标题',
             dataIndex: 'title',
@@ -145,6 +148,10 @@ class TableForGoods extends React.Component{
             title: '价格',
             dataIndex: 'price',
             key: 'price'
+        }, {
+            title: '库存',
+            dataIndex: 'stock',
+            key: 'stock'
         }, {
             title: '发布日期',
             dataIndex: 'publishDate',
@@ -156,22 +163,22 @@ class TableForGoods extends React.Component{
             filterMultiple: false,
             filters: [{
                 text: '衬衫',
-                value: ['clothes', 'shirt'],
+                value: 1,
             }, {
                 text: '夹克',
-                value: ['clothes', 'jacket'],
+                value: 2,
             }, {
                 text: '休闲裤',
-                value: ['pants', 'casual_pants'],
+                value: 3,
             }, {
                 text: '运动裤',
-                value: ['pants', 'sports_pants'],
+                value: 4,
             }, {
                 text: '篮球鞋',
-                value: ['shoes', 'basketball_shoes'],
+                value: 5,
             }, {
                 text: '休闲鞋',
-                value: ['shoes', 'casual_shoes'],
+                value: 6,
             }],
             // onFilter: (value, record) => {
             //     console.log(value, record);
@@ -206,6 +213,7 @@ class TableForGoods extends React.Component{
                     dataSource={this.state.data}
                     pagination={this.state.pagination}
                     onChange={this.handleTableChange}
+                    loading={this.state.loading}
                 />
             </div>
         )
