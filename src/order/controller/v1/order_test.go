41ff2d7c3248020be281eb4b95cf106d985d89b4
zhhnzw <747624368@qq.com>
2020-07-05 20:15:08 +0800

    拆分表结构，引入基础商品表和用户表，分割出订单表

--- src/order/controller/v1/order_test.go
+++ src/order/controller/v1/order_test.go
@@ -34,7 +34,7 @@ func Test(t *testing.T) {
 		Convey("Test GetOrderList", func() {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
-			request := &pb.OrderRequest{PageIndex:1, PageSize:10}
+			request := &pb.OrderRequest{PageIndex: 1, PageSize: 10}
 			reply, err := orderClient.GetOrderList(ctx, request)
 			So(err, ShouldBeNil)
 			utils.CheckRPCError(err)
@@ -44,16 +44,23 @@ func Test(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
 			request := &pb.OrderRequest{
-				GoodsUuid:"b7b10c01-62b8-42c7-a8c4-8efe119cd326",
-				PrimaryType:"clothes",
-				SecondaryType:"shirt",
-				GoodsTypeId:1,
-				Price:1931,
+				GoodsUuid:     "b7b10c01-62b8-42c7-a8c4-8efe119cd326",
+				PrimaryType:   "clothes",
+				SecondaryType: "shirt",
+				GoodsTypeId:   1,
+				Price:         1931,
+				Title:         "CYGNENOIR 20AW秋季字母印花宽松迷彩衬衫日系复古街头衬衣男潮",
+				Subtitle:      "衬衣 subtitle",
+				Img:           "http://shihuo.hupucdn.com/def/20200430/390bb16aeb93924ccd4c67f5f178cf9a1588213097.jpg",
+				UserName:      "user",
+				NickName:      "卖萌小公举",
+				Mobile:        "18321212121",
+				Email:         "2131@11.com",
 			}
 			reply, err := orderClient.CreateOrder(ctx, request)
+			log.Printf("%+v\n", reply)
 			So(err, ShouldBeNil)
 			utils.CheckRPCError(err)
-			log.Printf("%+v\n", reply)
 		})
 	})
 }
--- src/order/schema.sql
+++ src/order/schema.sql
@@ -1,4 +1,17 @@
-CREATE TABLE IF NOT EXISTS `tb_order` (
+CREATE TABLE IF NOT EXISTS `tb_goods_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `primary_type` enum('unknown','clothes', 'pants', 'shoes') DEFAULT 'unknown' COMMENT '一级商品类型,如: clothes(衣服)、pants(裤子)、shoes(鞋子)',
+  `secondary_type` enum('unknown','shirt','jacket','casual_pants', 'sports_pants', 'basketball_shoes','casual_shoes') DEFAULT 'unknown' COMMENT '二级商品类型,如: shirt(衬衫)、jacket(夹克)、casual_pants(休闲裤)、sports_pants(运动裤)、basketball_shoes(篮球鞋)、casual_shoes(休闲鞋)',
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted_time` timestamp NOT NULL DEFAULT '1970-01-01 08:00:01' COMMENT '删除时间, 逻辑删除',
+  PRIMARY KEY (`id`),
+  UNIQUE `unique_primary_secondary` (`primary_type`, `secondary_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品类型表';
+
+INSERT INTO `tb_goods_type` (`primary_type`, `secondary_type`) VALUES ('clothes', 'shirt'),('clothes', 'jacket'),('pants','casual_pants'),('pants','sports_pants'),('shoes','basketball_shoes'),('shoes','casual_shoes');
+
+CREATE TABLE IF NOT EXISTS `tb_goods` (
   `id` int NOT NULL AUTO_INCREMENT,
   `goods_uuid` char(36) NOT NULL COMMENT '商品id',
   `goods_type_id` int NOT NULL DEFAULT 0 COMMENT '商品类型id',
@@ -8,11 +21,42 @@ CREATE TABLE IF NOT EXISTS `tb_order` (
   `title` varchar(64) NOT NULL DEFAULT '' COMMENT '标题',
   `subtitle` varchar(256) NOT NULL DEFAULT '' COMMENT '子标题',
   `price` int NOT NULL DEFAULT 0 COMMENT '单位:厘. 存int类型精确到厘方便计算',
-  `order_status` enum('new','closed','paid','completed') DEFAULT 'new' COMMENT '订单状态: new(新建)、closed(已关闭)、paid(已支付)、completed(已完成)',
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted_time` timestamp NOT NULL DEFAULT '1970-01-01 08:00:01' COMMENT '删除时间, 逻辑删除',
   PRIMARY KEY (`id`),
+  UNIQUE `unique_goods_uuid` (`goods_uuid`),
   KEY `goods_type_id` (`goods_type_id`),
   KEY `primary_secondary_type` (`secondary_type`, `primary_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
+
+CREATE TABLE IF NOT EXISTS `tb_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(32) NOT NULL COMMENT '用户名,账号',
+  `nick_name` varchar(32) NOT NULL DEFAULT '' COMMENT '昵称',
+  `mobile` char(11) NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(32) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `avatar` varchar(128) NOT NULL DEFAULT '' COMMENT '头像地址',
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted_time` timestamp NOT NULL DEFAULT '1970-01-01 08:00:01' COMMENT '删除时间',
+  PRIMARY KEY (`id`),
+  UNIQUE `user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `tb_order` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` char(24) NOT NULL COMMENT '订单号',
+  `goods_uuid` char(36) NOT NULL COMMENT '商品id',
+  `goods_type_id` int NOT NULL DEFAULT 0 COMMENT '商品类型id',
+  `price` int NOT NULL DEFAULT 0 COMMENT '单位:厘. 存int类型精确到厘方便计算',
+  `order_status` enum('new','closed','paid','completed') DEFAULT 'new' COMMENT '订单状态: new(新建)、closed(已关闭)、paid(已支付)、completed(已完成)',
+  `user_name` varchar(32) NOT NULL COMMENT '用户名,账号',
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted_time` timestamp NOT NULL DEFAULT '1970-01-01 08:00:01' COMMENT '删除时间, 逻辑删除',
+  PRIMARY KEY (`id`),
+  UNIQUE `order_id` (`order_id`),
+  KEY `goods_type_id` (`goods_type_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单表';
+
