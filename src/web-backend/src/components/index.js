aea497f3c68dc5e298d933d8a7312fd83914a18a
zhhnzw <t>
2020-07-06 22:44:39 +0800

    添加订单模块

--- src/web-backend/src/components/index.js
+++ src/web-backend/src/components/index.js
@@ -2,10 +2,12 @@ import Home from './Home'
 import TableForGoods from './tables/TableForGoods'
 import LoginForm from './forms/LoginForm'
 import UserPopoverForm from './forms/UserPopoverForm'
+import TableForOrder from './tables/TableForOrder'
 
 export default {
     Home,
     TableForGoods,
+    TableForOrder,
     LoginForm,
     UserPopoverForm
 };
