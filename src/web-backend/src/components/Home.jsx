c31ee87fcf935389fb7ab99b70b5b07831bf2d1d
zhhnzw <t>
2021-03-05 16:43:44 +0800

    用highcharts替换bizcharts

--- src/web-backend/src/components/Home.jsx
+++ src/web-backend/src/components/Home.jsx
@@ -1,193 +1,28 @@
 import React from 'react'
-import {
-    Chart,
-    Interval,
-    Tooltip,
-    Axis,
-    Coordinate,
-    Interaction,
-    Annotation,
-    Guide,
-    Legend
-} from 'bizcharts';
-import {DataSet} from "@antv/data-set";
 import {get} from "../axios/tools";
 import {serviceDomain} from "../axios/config";
 import {Card} from "antd";
+import HighchartsReact from 'highcharts-react-official'
+var Highcharts = require('highcharts');
 
 
-class GoodsChart extends React.Component{
+class PieChart extends React.Component{
     render() {
-        const { DataView } = DataSet;
-        const { Html } = Guide;
-        const dv = new DataView();
-        dv.source(this.props.data).transform({
-            type: "percent",
-            field: "count",
-            dimension: "goodsType",
-            as: "percent"
-        });
-        const cols = {
-            percent: {
-                formatter: val =>{
-                    val = (val*100).toFixed(2)+"%";
-                    return val;
-                }
-            }
-        };
-        // let guideHtml = '<div style="color:#8c8c8c;font-size: 1.16em;text-align: center;width: 10em;">总计<br/><span style="color: #262626;font-size: 1.8em">' + this.props.total + '</span>个</div>';
-        return (
-            <div>
-                <h1>商品类型分布</h1>
-                <Chart
-                    height={400}
-                    data={dv}
-                    scale={cols}
-                    autoFit
-                >
-                    <Legend visible={false} />
-                    <Coordinate type="theta" innerRadius={0.4}/>
-                    <Axis visible={false}/>
-                    <Interval
-                        position="percent"
-                        adjust="stack"
-                        color="goodsType"
-                        style={{
-                            lineWidth: 1,
-                            stroke: '#fff',
-                        }}
-                        label={['count', {
-                            content: (data) => {
-                                return `${data.goodsType}: ${(data.percent * 100).toFixed(2)}%`;
-                            },
-                        }]}
-                    />
-                    {/*<Guide>*/}
-                    {/*    <Html*/}
-                    {/*        position={["50%", "50%"]}*/}
-                    {/*        html={guideHtml}*/}
-                    {/*        alignX="middle"*/}
-                    {/*        alignY="middle"*/}
-                    {/*    />*/}
-                    {/*</Guide>*/}
-                    <Annotation.Text
-                        position={['50%', '50%']}
-                        content={this.props.total}
-                        style={{
-                            lineHeight: '240px',
-                            fontSize: '30',
-                            fill: '#262626',
-                            textAlign: 'center',
-                        }}
-                    />
-                    <Interaction type='element-single-selected' />
-                    {/*<Legend*/}
-                    {/*    position="right"*/}
-                    {/*    offsetY={-window.innerHeight/2+230}*/}
-                    {/*    offsetX={-20}*/}
-                    {/*/>*/}
-                    {/*<Tooltip*/}
-                    {/*    showTitle={false}*/}
-                    {/*    itemTpl="<li><span style=&quot;background-color:{color};&quot; class=&quot;g2-tooltip-market&quot;></span>{name}: {value}</li>"*/}
-                    {/*/>*/}
-                    {/*<Guide>*/}
-                    {/*    <Html*/}
-                    {/*        position={["50%", "50%"]}*/}
-                    {/*        html={guideHtml}*/}
-                    {/*        alignX="middle"*/}
-                    {/*        alignY="middle"*/}
-                    {/*    />*/}
-                    {/*</Guide>*/}
-                    {/*<Geom*/}
-                    {/*    type="intervalStack"*/}
-                    {/*    postion="count"*/}
-                    {/*    color="item"*/}
-                    {/*    style={{*/}
-                    {/*        lineWidth: 1,*/}
-                    {/*        stroke: "#fff"*/}
-                    {/*    }}*/}
-                    {/*>*/}
-                    {/*    <Label*/}
-                    {/*        content="percent"*/}
-                    {/*        formatter={(val, item) => {*/}
-                    {/*            return item.point.item + ": " + val*/}
-                    {/*        }}*/}
-                    {/*    />*/}
-                    {/*</Geom>*/}
-                </Chart>
-            </div>
-        );
-    }
-}
-
-class OrderChart extends React.Component{
-    render() {
-        const { DataView } = DataSet;
-        const dv = new DataView();
-        dv.source(this.props.data).transform({
-            type: "percent",
-            field: "count",
-            dimension: "goodsType",
-            as: "percent"
-        });
-        const cols = {
-            percent: {
-                formatter: val =>{
-                    val = (val*100).toFixed(2)+"%";
-                    return val;
-                }
-            }
-        };
-        return (
-            <div>
-                <h1>订单类型分布</h1>
-                <Chart
-                    height={400}
-                    data={dv}
-                    scale={cols}
-                    autoFit
-                >
-                    <Legend visible={false} />
-                    <Coordinate type="theta" innerRadius={0.4}/>
-                    <Axis visible={false}/>
-                    <Interval
-                        position="percent"
-                        adjust="stack"
-                        color="goodsType"
-                        style={{
-                            lineWidth: 1,
-                            stroke: '#fff',
-                        }}
-                        label={['count', {
-                            content: (data) => {
-                                return `${data.goodsType}: ${(data.percent * 100).toFixed(2)}%`;
-                            },
-                        }]}
-                    />
-                    <Annotation.Text
-                        position={['50%', '50%']}
-                        content={this.props.total}
-                        style={{
-                            lineHeight: '240px',
-                            fontSize: '30',
-                            fill: '#262626',
-                            textAlign: 'center',
-                        }}
-                    />
-                    <Interaction type='element-single-selected' />
-                </Chart>
-            </div>
-        );
+        return <div>
+            <HighchartsReact
+                highcharts={Highcharts}
+                constructorType={'chart'}
+                options={this.props.options}
+            />
+        </div>
     }
 }
 
 class Home extends React.Component{
     state = {
         userTotal: 0,
-        goodsData: [],
-        goodsTotal: 0,
-        orderData: [],
-        orderTotal: 0,
+        goodsData: {"title": {"text": ""}, "series": []},
+        orderData: {"title": {"text": ""}, "series": []},
     };
 
     componentDidMount() {
@@ -202,34 +37,67 @@ class Home extends React.Component{
                 this.setState({userTotal:d.data.data.count})
             }
         });
+        let options = {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: []
+        };
+
         get({
             url: serviceDomain + '/v1/statistic/goods',
             callback: (d) => {
-                let count = 0;
-                for (let i in d.data.data.data) {
-                    let c = d.data.data.data[i].count;
-                    if (c!==undefined){
-                        count += c;
-                    }else {
-                        d.data.data.data[i].count = 0;
-                    }
-                }
-                this.setState({goodsData: d.data.data.data, goodsTotal:count})
+                let targetOptions = {...options};
+                targetOptions.title.text = '商品类型分布';
+                let data = [];
+                d.data.data.data.forEach(one => {
+                    data.push({'name': one.goodsType, 'y': one.count!==undefined?one.count:0})
+                });
+                targetOptions.series.push({
+                    name: 'goodsType',
+                    colorByPoint: true,
+                    data: data});
+                console.log(targetOptions);
+                this.setState({goodsData: targetOptions});
             }
         });
         get({
             url: serviceDomain + '/v1/statistic/order',
             callback: (d) => {
-                let count = 0;
-                for (let i in d.data.data.data) {
-                    let c = d.data.data.data[i].count;
-                    if (c!==undefined){
-                        count += c;
-                    }else {
-                        d.data.data.data[i].count = 0;
-                    }
-                }
-                this.setState({orderData: d.data.data.data, orderTotal:count})
+                let targetOptions = {...options};
+                targetOptions.title.text = '订单类型分布';
+                let data = [];
+                d.data.data.data.forEach(one => {
+                    data.push({'name': one.goodsType, 'y': one.count!==undefined?one.count:0})
+                });
+                targetOptions.series.push({
+                    name: 'goodsType',
+                    colorByPoint: true,
+                    data: data});
+                console.log(targetOptions);
+                this.setState({orderData: targetOptions});
             }
         })
     }
@@ -260,12 +128,12 @@ class Home extends React.Component{
                 <div style={{fontSize:'24px', marginLeft:'20px', marginTop:'20px', height:'500px', width:window.innerWidth-272}}>
                     <div style={{width:(window.innerWidth-278)/2, height:'500px', display:'inline-block'}}>
                         <Card style={{width:(window.innerWidth-278)/2, height:'500px'}}>
-                            <GoodsChart data={this.state.goodsData} total={this.state.goodsTotal}/>
+                            <PieChart options={this.state.goodsData} total={this.state.goodsTotal}/>
                         </Card>
                     </div>
                     <div style={{width:100, height:'500px', display:'inline-block', marginLeft:'8px'}}>
                         <Card style={{width:(window.innerWidth-278)/2, height:'500px'}}>
-                            <OrderChart data={this.state.orderData} total={this.state.orderTotal}/>
+                            <PieChart options={this.state.orderData} total={this.state.orderTotal}/>
                         </Card>
                     </div>
                 </div>
